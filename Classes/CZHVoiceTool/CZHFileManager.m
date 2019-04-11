b7931e5d814088b6a343a7934c4cc40167292f64
李腾飞 <litengfei@winkind-tech.com>
2019-04-11 15:34:37 +0800

    update

--- Classes/CZHVoiceTool/CZHFileManager.m
+++ Classes/CZHVoiceTool/CZHFileManager.m
@@ -1,15 +1,15 @@
-//
-//  CZHFileManager.m
-//  CZHRecordVoice
-//
-//  Created by 程召华 on 2018/9/3.
-//  Copyright © 2018年 程召华. All rights reserved.
-//
+    //
+    //  CZHFileManager.m
+    //  CZHRecordVoice
+    //
+    //  Created by 程召华 on 2018/9/3.
+    //  Copyright © 2018年 程召华. All rights reserved.
+    //
 
 #define LOCAL_AUDIO_FOLDER @"CZHVoice"
 
 #import "CZHFileManager.h"
-//#import "amrFileCodec.h"
+    //#import "amrFileCodec.h"
 
 @implementation CZHFileManager
 
@@ -38,22 +38,22 @@
 
 
 
-//转换为amr格式并生成文件到savePath(showSize是否在控制台打印转换后的文件大小)
-//+ (NSString *)czh_convertWavtoAMRWithVoiceFilePath:(NSString *)voiceFilePath{
-//
-//    NSData *data = [NSData dataWithContentsOfFile:voiceFilePath];
-//
-//    data = EncodeWAVEToAMR(data,1,16);
-//
-//    NSString *amrFilePath = [voiceFilePath stringByReplacingOccurrencesOfString:@"wav" withString:@"amr"];
-//
-//    BOOL isSuccess=[data writeToURL:[NSURL fileURLWithPath:amrFilePath] atomically:YES];
-//
-//    if (isSuccess) {
-//        NSLog(@"CDPAudioRecorder转换为amr格式成功,大小为%@",[self czh_fileSizeAtPath:amrFilePath]);
-//    }
-//    return amrFilePath;
-//}
+    //转换为amr格式并生成文件到savePath(showSize是否在控制台打印转换后的文件大小)
+    //+ (NSString *)czh_convertWavtoAMRWithVoiceFilePath:(NSString *)voiceFilePath{
+    //
+    //    NSData *data = [NSData dataWithContentsOfFile:voiceFilePath];
+    //
+    //    data = EncodeWAVEToAMR(data,1,16);
+    //
+    //    NSString *amrFilePath = [voiceFilePath stringByReplacingOccurrencesOfString:@"wav" withString:@"amr"];
+    //
+    //    BOOL isSuccess=[data writeToURL:[NSURL fileURLWithPath:amrFilePath] atomically:YES];
+    //
+    //    if (isSuccess) {
+    //        NSLog(@"CDPAudioRecorder转换为amr格式成功,大小为%@",[self czh_fileSizeAtPath:amrFilePath]);
+    //    }
+    //    return amrFilePath;
+    //}
 + (NSString*)czh_saveFileWithURL:(NSString *)fileUrl withFileName:(NSString *)fileName {
     NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:fileUrl]];
     NSString *folderPath = [CZHFileManager czh_folderPath];
@@ -71,7 +71,9 @@
 + (NSString *)czh_saveWavWithVoiceUrl:(NSString *)voiceUrl {
     
     NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:voiceUrl]];
-    
+    if (data ==nil ||data.length == 0 ){
+        NSLog(@"文件 %@ 下载失败",voiceUrl);
+    }
     NSString *folderPath = [CZHFileManager czh_folderPath];
     
     NSString *wavPath = [folderPath stringByAppendingPathComponent:[voiceUrl lastPathComponent]];
@@ -86,53 +88,53 @@
         return @"";
     }
 }
-//+ (NSString *)czh_convertAMRToWavWithVoiceFilePath:(NSString *)voiceFilePath {
-//    NSData *data = [NSData dataWithContentsOfFile:voiceFilePath];
-//
-//    data = DecodeAMRToWAVE(data);
-//
-//    NSString *folderPath = [CZHFileManager czh_folderPath];
-//
-//    NSString *wavPath = [folderPath stringByAppendingPathComponent:[voiceFilePath lastPathComponent]];
-//
-//    wavPath = [wavPath stringByReplacingOccurrencesOfString:@"amr" withString:@"wav"];
-//
-//    BOOL isSuccess = [data writeToFile:wavPath atomically:YES];
-//
-//    if (isSuccess) {
-//        NSLog(@"保存成功%@",wavPath);
-//        return wavPath;
-//    } else {
-//        NSLog(@"保存失败");
-//        return @"";
-//    }
-//}
-//
-//
-//
-//
-//+ (NSString *)czh_convertAMRToWavWithVoiceUrl:(NSString *)voiceUrl {
-//
-//    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:voiceUrl]];
-//
-//    data = DecodeAMRToWAVE(data);
-//
-//    NSString *folderPath = [CZHFileManager czh_folderPath];
-//
-//    NSString *wavPath = [folderPath stringByAppendingPathComponent:[voiceUrl lastPathComponent]];
-//
-//    wavPath = [wavPath stringByReplacingOccurrencesOfString:@"amr" withString:@"wav"];
-//
-//    BOOL isSuccess = [data writeToFile:wavPath atomically:YES];
-//
-//    if (isSuccess) {
-//        NSLog(@"保存成功%@",wavPath);
-//        return wavPath;
-//    } else {
-//        NSLog(@"保存失败");
-//        return @"";
-//    }
-//}
+    //+ (NSString *)czh_convertAMRToWavWithVoiceFilePath:(NSString *)voiceFilePath {
+    //    NSData *data = [NSData dataWithContentsOfFile:voiceFilePath];
+    //
+    //    data = DecodeAMRToWAVE(data);
+    //
+    //    NSString *folderPath = [CZHFileManager czh_folderPath];
+    //
+    //    NSString *wavPath = [folderPath stringByAppendingPathComponent:[voiceFilePath lastPathComponent]];
+    //
+    //    wavPath = [wavPath stringByReplacingOccurrencesOfString:@"amr" withString:@"wav"];
+    //
+    //    BOOL isSuccess = [data writeToFile:wavPath atomically:YES];
+    //
+    //    if (isSuccess) {
+    //        NSLog(@"保存成功%@",wavPath);
+    //        return wavPath;
+    //    } else {
+    //        NSLog(@"保存失败");
+    //        return @"";
+    //    }
+    //}
+    //
+    //
+    //
+    //
+    //+ (NSString *)czh_convertAMRToWavWithVoiceUrl:(NSString *)voiceUrl {
+    //
+    //    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:voiceUrl]];
+    //
+    //    data = DecodeAMRToWAVE(data);
+    //
+    //    NSString *folderPath = [CZHFileManager czh_folderPath];
+    //
+    //    NSString *wavPath = [folderPath stringByAppendingPathComponent:[voiceUrl lastPathComponent]];
+    //
+    //    wavPath = [wavPath stringByReplacingOccurrencesOfString:@"amr" withString:@"wav"];
+    //
+    //    BOOL isSuccess = [data writeToFile:wavPath atomically:YES];
+    //
+    //    if (isSuccess) {
+    //        NSLog(@"保存成功%@",wavPath);
+    //        return wavPath;
+    //    } else {
+    //        NSLog(@"保存失败");
+    //        return @"";
+    //    }
+    //}
 
 
 + (NSString *)czh_voiceUrlIsExistInLocalWithLastPathComponent:(NSString *)lastPathComponent {
@@ -141,7 +143,7 @@
     
     NSString *wavPath = [folderPath stringByAppendingPathComponent:lastPathComponent];
     
-    //如果后缀是amr格式转成wav
+        //如果后缀是amr格式转成wav
     if ([lastPathComponent hasSuffix:@"amr"]) {
         
         wavPath = [wavPath stringByReplacingOccurrencesOfString:@"amr" withString:@"wav"];
@@ -160,7 +162,7 @@
 }
 
 
-//查看文件大小(iOS是按照1000换算的,而不是1024,可查看NSByteCountFormatterCountStyle)
+    //查看文件大小(iOS是按照1000换算的,而不是1024,可查看NSByteCountFormatterCountStyle)
 + (NSString *)czh_fileSizeAtPath:(NSString*)filePath{
     unsigned long long size=0;
     
@@ -169,16 +171,16 @@
         size=[[manager attributesOfItemAtPath:filePath error:nil] fileSize];
         
         if (size >=pow(10,9)) {
-            // size >= 1GB
+                // size >= 1GB
             return [NSString stringWithFormat:@"%.2fGB",size/pow(10,9)];
         } else if (size>=pow(10,6)) {
-            // 1GB > size >= 1MB
+                // 1GB > size >= 1MB
             return [NSString stringWithFormat:@"%.2fMB",size/pow(10,6)];
         } else if (size >=pow(10,3)) {
-            // 1MB > size >= 1KB
+                // 1MB > size >= 1KB
             return [NSString stringWithFormat:@"%.2fKB",size/pow(10,3)];
         } else {
-            // 1KB > size
+                // 1KB > size
             return [NSString stringWithFormat:@"%zdB",size];
         }
     }
